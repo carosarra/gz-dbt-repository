9c5288f88d890eddeec5e64be73fcd1fb4b022e9
karosarrazin33@gmail.com <karosarrazin33@gmail.com>
2024-02-06 15:09:06 +0000

    calcul_margin_purchase_cost

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,4 +1,7 @@
 select
-    (quantity * purchase_price) as purchase_cost
+    orders_id
+    ,round(purchase_price*quantity,2) as purchase_cost
+    ,round(revenue - (purchase_price*quantity),2) as margin
+    
 from {{ ref('stg_raw__sales') }} 
 join {{ ref('stg_raw__product') }} using(products_id)
