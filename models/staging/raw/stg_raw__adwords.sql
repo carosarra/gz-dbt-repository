22604cf73ec31b6f1645c342dc0af5d84500a720
Caroline Sarrazin <karosarrazin33@gmail.com>
2024-02-07 13:08:34 +0000

    add raw add sources

--- models/staging/raw/stg_raw__adwords.sql
+++ models/staging/raw/stg_raw__adwords.sql
@@ -12,8 +12,8 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name, as campaign_name
-        CAST(ads_cost AS FLOAT 64), as ads_cost
+        campgn_name as campaign_name,
+        CAST(ads_cost AS FLOAT64) as ads_cost,
         impression,
         click
 
