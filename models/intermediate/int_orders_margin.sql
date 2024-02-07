a113c12d5983328d91689fc159c5724f7671746e
Caroline Sarrazin <karosarrazin33@gmail.com>
2024-02-07 14:58:11 +0000

    macro executed

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -5,6 +5,7 @@ orders_id
 ,round(sum(quantity),1) as quantity
 ,round(sum(purchase_cost),2) as purchase_cost
 ,round(sum(margin),2) as margin
+
 from {{ref('int_sales_margin')}}
 group by orders_id, date_date
 order by orders_id DESC
--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,16 +1,29 @@
+WITH table AS(
+
+            SELECT
+                orders_id
+                ,date_date
+                ,revenue
+                ,quantity
+                ,round(purchase_price*quantity,2) as purchase_cost
+                ,round(revenue - (purchase_price*quantity),2) as margin
+
+
+            FROM{{ ref('stg_raw__sales') }}
+            JOIN {{ ref('stg_raw__product') }}
+            USING(products_id)
+            )
+
 SELECT
     orders_id
     ,date_date
     ,revenue
     ,quantity
-    ,round(purchase_price*quantity,2) as purchase_cost
-    ,round(revenue - (purchase_price*quantity),2) as margin
+    ,purchase_cost
+    ,margin
     ,{{ margin_percent('revenue','purchase_cost') }} as margin_percent
 
-
-FROM{{ ref('stg_raw__sales') }}
-JOIN {{ ref('stg_raw__product') }}
-USING(products_id)
+FROM table
 
 
 
